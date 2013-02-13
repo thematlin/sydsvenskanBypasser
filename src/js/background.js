79bbb16a858d55e140b164ca8421ad0295363365
Mattias Lindahl <mattias@mlindahl.nu>
2013-02-13 18:59:55 +0100

    Extension is now only run in background. No interaction required

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,20 @@
 var checkForValidUrl = function(tabId, changeInfo, tab) {
 	if (tab.url.indexOf('sydsvenskan.se') > -1) {
-		chrome.pageAction.show(tabId);
+		chrome.cookies.get({ url: sydsvenskanUrl, name: cookieName }, getSydsvenskanCookieCallback);
 	}
 };
+
+var getSydsvenskanCookieCallback = function(cookie) {
+	if (cookie !== null) {
+		resetArticlesCookieAndShowSuccess(cookie);
+	}
+};
+
+var resetArticlesCookieAndShowSuccess = function(cookie) {
+	chrome.cookies.remove({ url: sydsvenskanUrl, name: cookieName });
+};
+
+var sydsvenskanUrl = 'http://www.sydsvenskan.se';
+var cookieName = '.visitedPages';
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
